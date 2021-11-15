c513e8a23cb3403630e4766f3e90e4e991b327e1
DerTyp187 <janis.meister87@gmail.com>
2021-11-15 11:30:35 +0100

    Create Wuerfeln.java

---
+++ src/zweidimnesionaleArrays/Wuerfeln.java
@@ -0,0 +1,27 @@
+package zweidimnesionaleArrays;
+
+import java.io.IOException;
+
+
+public class Wuerfeln {
+    public static void main(String[] args) throws IOException {
+        int anzahlWuerfe = 10;
+        int anzahlExperimente = 4;
+        int[][] wuerfe = new int[anzahlExperimente][anzahlWuerfe];
+
+        for(int i = 0; i < anzahlExperimente; i++){
+            for(int a = 0; a < anzahlWuerfe; a++){
+                wuerfe[i][a] = (int)(Math.random()*6)+1;
+            }
+        }
+
+
+
+        for(int i = 0; i < anzahlExperimente; i++){
+            for(int a = 0; a < anzahlWuerfe; a++){
+                System.out.print(wuerfe[i][a] +" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
