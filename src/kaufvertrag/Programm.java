4da344ec73e26025f1287206ff435f2782847f54
Janis <janis.meister87@gmail.com>
2022-05-23 10:32:17 +0200

    r

---
+++ src/kaufvertrag/Programm.java
@@ -0,0 +1,19 @@
+package kaufvertrag;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Programm {
+    public static void main(String[] args) throws FileNotFoundException {
+        Vertragspartner kaeufer = new Vertragspartner("Julius", "Ewert");
+        Vertragspartner verkaeufer = new Vertragspartner("Noah", "Thiering");
+        Ware w = new Ware("Stock", 10000.2);
+        Kaufvertrag k = new Kaufvertrag(kaeufer, verkaeufer, w);
+        System.out.println(k.toString());
+        String datei = "src/kaufvertrag/Vertrag.csv";
+        PrintWriter writer = new PrintWriter(datei);
+        writer.println(k.toString());
+        writer.close();
+
+    }
+}
